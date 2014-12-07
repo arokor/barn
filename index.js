09fa480c94fae9dc7db5a2a0ccb69d93ff5d9323
Aron Kornhall <git@kornhall.se>
2014-12-07 13:56:13 +0100

    Initial barn

---
+++ index.js
@@ -0,0 +1,48 @@
+var _ = require('underscore');
+var Barn = require('./lib/barn');
+
+function API(namespace, storage){
+  if(!storage){
+    storage = namespace;
+    namespace = 'BARN';
+  }
+  this.barn = new Barn(namespace, storage);
+}
+
+_.extend(API.prototype, {
+  get: function(key){
+    return this.barn.execCmd('GET', key);
+  },
+  set: function(key, val){
+    return this.barn.execCmd('SET', key, val);
+  },
+  del: function(key){
+    return this.barn.execCmd('DEL', key);
+  },
+  lpop: function(key){
+    return this.barn.execCmd('LPOP', key);
+  },
+  lpush: function(key, val){
+    return this.barn.execCmd('LPUSH', key, val);
+  },
+  rpop: function(key){
+    return this.barn.execCmd('RPOP', key);
+  },
+  rpush: function(key, val){
+    return this.barn.execCmd('RPUSH', key, val);
+  },
+  sadd: function(key, val){
+    return this.barn.execCmd('SADD', key, val);
+  },
+  smembers: function(key){
+    return this.barn.execCmd('SMEMBERS', key);
+  },
+  srem: function(key, val){
+    return this.barn.execCmd('SREM', key, val);
+  },
+  condense: function(){
+    return this.barn.condense();
+  },
+});
+
+module.exports = API;
---
+++ test/barn.test.js
@@ -0,0 +1,232 @@
+var Barn = require('../index');
+var expect = require('chai').expect;
+
+function createMockStorage(){
+  return {
+    s:{},
+    length: 0,
+    getItem: function(key){
+      return (key in this.s) ? this.s[key] : null;
+    },
+    setItem: function(key, val){
+      if(!(key in this.s)) this.length++;
+      this.s[key] = ''+val;
+    },
+    removeItem: function(key){
+      if(key in this.s) this.length--;
+      delete this.s[key];
+    },
+    key: function(idx){
+      return Object.keys(this.s)[idx] || null;
+    },
+    clear: function(){
+      this.length = 0;
+      var _this = this;
+      Object.keys(this.s).forEach(function(key){
+        _this.removeItem(key);
+      });
+    }
+  };
+}
+
+
+describe('barn', function(){
+  var barn;
+  var storage;
+  beforeEach(function(){
+    storage = createMockStorage();
+    barn = new Barn(storage);
+  });
+  describe('string operations', function(){
+    it('set-get', function(){
+      barn.set('key', 'val');
+      barn.set('key2', 'val2');
+      expect(barn.get('key')).to.equal('val');
+      expect(barn.get('key2')).to.equal('val2');
+    });
+    describe('del', function(){
+      it('del', function(){
+        barn.set('key', 'val');
+        expect(barn.get('key')).to.equal('val');
+        expect(barn.del('key')).to.equal(1);
+        expect(barn.get('key')).to.be.null;
+      });
+      it('del non existent key', function(){
+        expect(barn.del('key')).to.equal(0);
+      });
+    });
+  });
+  describe('List operations', function(){
+    describe('LPUSH', function(){
+      it('LPUSH', function(){
+        var len = barn.lpush('key', 'val');
+        expect(len).to.equal(1);
+        len = barn.lpush('key', 'val2');
+        expect(len).to.equal(2);
+      });
+      it('LPUSH on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.lpush('key', 'val');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('LPOP', function(){
+      it('LPOP', function(){
+        var len = barn.lpush('key', 'val1');
+        expect(len).to.equal(1);
+        len = barn.lpush('key', 'val2');
+        expect(len).to.equal(2);
+        expect(barn.lpop('key')).to.equal('val2');
+        expect(barn.lpop('key')).to.equal('val1');
+      });
+      it('LPOP on non existent list', function(){
+        expect(barn.lpop('key')).to.be.null;
+      });
+      it('LPOP underflow', function(){
+        var len = barn.lpush('key', 'val1');
+        expect(len).to.equal(1);
+        expect(barn.lpop('key')).to.equal('val1');
+        expect(barn.lpop('key')).to.be.null;
+      });
+      it('LPOP on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.lpop('key');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('RPUSH', function(){
+      it('RPUSH', function(){
+        var len = barn.rpush('key', 'val');
+        expect(len).to.equal(1);
+        len = barn.rpush('key', 'val2');
+        expect(len).to.equal(2);
+      });
+      it('RPUSH on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.rpush('key', 'val');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('RPOP', function(){
+      it('RPOP', function(){
+        var len = barn.rpush('key', 'val1');
+        expect(len).to.equal(1);
+        len = barn.rpush('key', 'val2');
+        expect(len).to.equal(2);
+        expect(barn.rpop('key')).to.equal('val2');
+        expect(barn.rpop('key')).to.equal('val1');
+      });
+      it('RPOP on non existent list', function(){
+        expect(barn.rpop('key')).to.be.null;
+      });
+      it('RPOP underflow', function(){
+        var len = barn.rpush('key', 'val1');
+        expect(len).to.equal(1);
+        expect(barn.rpop('key')).to.equal('val1');
+        expect(barn.rpop('key')).to.be.null;
+      });
+      it('RPOP on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.rpop('key');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('SADD', function(){
+      it('SADD', function(){
+        var numAdded = barn.sadd('key', 'val');
+        expect(numAdded).to.equal(1);
+      });
+      it('returns number of added vals', function(){
+        var numAdded = barn.sadd('key', 'val');
+        expect(numAdded).to.equal(1);
+        numAdded = barn.sadd('key', 'val2');
+        expect(numAdded).to.equal(1);
+        numAdded = barn.sadd('key', 'val2');
+        expect(numAdded).to.equal(0);
+      });
+      it('on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.sadd('key', 'val');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('SMEMBERS', function(){
+      it('SMEMBERS', function(){
+        barn.sadd('key', 'val1');
+        expect(barn.smembers('key').length).to.equal(1);
+        barn.sadd('key', 'val2');
+        expect(barn.smembers('key').length).to.equal(2);
+        barn.sadd('key', 'val2');
+        expect(barn.smembers('key').length).to.equal(2);
+      });
+      it('SMEMBERS on non existent set', function(){
+        expect(barn.smembers('key')).to.be.null;
+      });
+      it('SMEMBERS on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.smebers('key');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+    describe('SREM', function(){
+      it('SREM', function(){
+        barn.sadd('key', 'val');
+        var numRemoved = barn.srem('key', 'val');
+        expect(numRemoved).to.equal(1);
+      });
+      it('returns number of removed vals', function(){
+        barn.sadd('key', 'val');
+        barn.sadd('key', 'val1');
+        var numRemoved = barn.srem('key', 'val');
+        expect(numRemoved).to.equal(1);
+        numRemoved = barn.srem('key', 'val');
+        expect(numRemoved).to.equal(0);
+      });
+      it('on string val', function(){
+        barn.set('key', 'val');
+        expect(function(){barn.srem('key', 'val');}).to.throw(TypeError);
+        expect(barn.get('key')).to.equal('val');
+      });
+    });
+  });
+  describe('Persistence', function(){
+    it('set-get', function(){
+      barn.set('key', 'val');
+      barn.set('key2', 'val2');
+      var barn2 = new Barn(storage);
+      expect(barn2.get('key')).to.equal('val');
+      expect(barn2.get('key2')).to.equal('val2');
+    });
+    describe('Condensation', function(){
+      it('should have the same db after condense', function(){
+        barn.set('key', 'val');
+        barn.condense();
+        var barn2 = new Barn(storage);
+        barn2.set('key2', 'val2');
+        barn2.condense();
+        var barn3 = new Barn(storage);
+        barn3.set('key3', 'val3');
+        expect(barn3.get('key')).to.equal('val');
+        expect(barn3.get('key2')).to.equal('val2');
+        expect(barn3.get('key3')).to.equal('val3');
+      });
+      it('should have only one key after condensation', function(){
+        barn.set('key', 'val');
+        barn.set('key1', 'val1');
+        barn.set('key2', 'val2');
+        expect(storage.length).to.equal(3 + 1);
+        barn.condense();
+        expect(storage.length).to.equal(1 + 1);
+      });
+      it('should autocondense when maxKeys is reached', function(){
+        expect(storage.length).to.equal(1);
+        for(var i=0; i<1000; i++){
+          barn.set('key'+i, 'val'+i);
+        }
+        expect(storage.length).to.equal(1000 + 1);
+        barn.set('keyX', 'valX');
+        expect(storage.length).to.equal(1 + 1);
+      });
+    });
+  });
+});
